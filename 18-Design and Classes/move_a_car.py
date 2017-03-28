9405074d1d1ae17d218d65b9fdbb2b6ac48a2297
Kevin-Robert <kevin.cox980@gmail.com>
2017-03-28 12:07:18 -0400

    updates for network are working slow today

--- 18-Design and Classes/move_a_car.py
+++ 18-Design and Classes/move_a_car.py
@@ -3,25 +3,28 @@ import sys
 sys.path.append('C:/Users/Owner/Documents/0Senior Uk/CE599/ce599-s17/18-Design and Classes/transport')
 
 #calling the cars import
-from Car import Car
+from Cars import Car
 
 if __name__=="__main__":
 	
-	Cary= Car(color= 'yellow', location=[0,0],direction='F')
-	Cary.printcar()
-	Cary.go(2,'forward')
-	Cary.turn_left()
-	Cary.go(1,'forward')
-	Cary.printcar()
+	print('first car')
+	cary= Car(color= 'yellow', location=[0,0],direction='F')
+	cary.printcar()
+	cary.go(2,'forward')
+	cary.turn_left()
+	cary.go(1,'forward')
+	cary.printcar()
 
 	
+from Car import Car
+
+
+Carg=Car(color= 'green', location=[0,0],direction='F')
+Carg.printcar()
+Carg.left_turn()
+Carg.go(1,'forward')
+Carg.right_turn()
+Carg.go(2,'forward')
+Carg.printcar()
 	
-	Carg=Car(color= 'green', location=[0,0],direction='F')
-	Carg.printcar()
-	Carg.left_turn()
-	Carg.go(1,'forward')
-	Carg.right_turn()
-	Carg.go(2,'forward')
-	Carg.printcar()
-	
-	print('Are you a Packers fan for picking yellow and green cars?')
+print('Are you a Packers fan for picking yellow and green cars?')
